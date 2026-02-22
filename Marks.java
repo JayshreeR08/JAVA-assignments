833336b9363b288c836e23ea0c26d34449ced807
rider <jayshreekanwar1001@gmail.com>
2026-02-22 13:19:28 +0530

    Add files via upload

---
+++ Marks.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Marks
+{
+public static void main(String[] args)
+{
+Scanner S=new Scanner(System.in);
+int number=S.nextInt();
+if(number<30)
+{
+System.out.println("fail");
+}
+else
+{
+System.out.println("pass");
+}
+}
+}
