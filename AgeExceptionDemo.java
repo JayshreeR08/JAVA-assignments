20f791d41ccf8b2875fa759f6e653fa8b6446af0
rider <jayshreekanwar1001@gmail.com>
2026-04-27 19:16:51 +0530

    Create AgeExceptionDemo.java

---
+++ AgeExceptionDemo.java
@@ -0,0 +1,20 @@
+class AgeException extends Exception {
+    AgeException(String msg) {
+        super(msg);
+    }
+}
+
+public class AgeExceptionDemo {
+    static void checkAge(int age) throws AgeException {
+        if(age < 18)
+            throw new AgeException("Not eligible");
+    }
+
+    public static void main(String[] args) {
+        try {
+            checkAge(16);
+        } catch(Exception e) {
+            System.out.println(e);
+        }
+    }
+}
