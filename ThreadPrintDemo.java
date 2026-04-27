91347c31cf2406f3292a9b933feac84737fa37ca
rider <jayshreekanwar1001@gmail.com>
2026-04-27 18:07:21 +0530

    Create ThreadPrintDemo.java

---
+++ ThreadPrintDemo.java
@@ -0,0 +1,19 @@
+class MyThread extends Thread {
+    public void run() {
+        for(int i=1; i<=100; i++) {
+            System.out.println(Thread.currentThread().getName() + " : " + i);
+        }
+    }
+}
+
+public class ThreadPrintDemo {
+    public static void main(String[] args) {
+        MyThread t1 = new MyThread();
+        MyThread t2 = new MyThread();
+        MyThread t3 = new MyThread();
+
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
